d8e00d0155c03fa3c872f4c3023e2db7d7bb4d63
Amanda Prates (Jubilly) <amandaprates-12@hotmail.com>
2021-08-03 20:51:54 -0300

    Inverte palavras - pilha

---
+++ questao1/questao1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void inverte_palavras(string palavras)
+{
+    stack<char> pilha;
+    int i = 0,
+        pilha_tamanho,
+        palavras_tamanho = palavras.length();
+    while (palavras[i] != '\0')
+    {
+        if(palavras[i] !=' '){
+            pilha_tamanho = pilha.size();
+            pilha.push(palavras[i]);
+        }else{
+            for(int j=0; j <= pilha_tamanho;j++ ){
+                cout << pilha.top();
+                pilha.pop(); 
+            }
+            cout<< " ";
+        }
+        i++;
+    }
+    for(int k = 0; k <= pilha_tamanho;k++ ){
+        cout << pilha.top();
+        pilha.pop(); 
+    }
+
+}
+int main()
+{
+    string frase;
+    cout << "Digite uma frase: ";
+    getline(cin, frase);
+    inverte_palavras(frase);
+}
